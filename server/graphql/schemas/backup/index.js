be39488bc77d728d8207607aabe59f36dacc0723
Ping <pinghe2022@u.northwestern.edu>
2024-07-15 13:31:36 -0400

    Update index.js

--- server/graphql/schemas/backup/index.js
+++ server/graphql/schemas/backup/index.js
@@ -1,8 +1,8 @@
 // TODO: npm install graphql-tag @apollo/subgraph @apollo/server 
 // use apollo instead of express
 import documentSchema from './document.js';
-import mailHistorySchema from '../mailHistory.js';
-import visaStatusSchema from '../visaStatus.js';
+import mailHistorySchema from './mailHistory.js';
+import visaStatusSchema from './visaStatus.js';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
 const linkSchema = /* GraphQL */ `
