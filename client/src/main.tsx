9b8194991c89698a47e01df0317136eaa7fd6791
Yanxuan Wang <wang.yanxuan2023@gmail.com>
2024-07-17 21:15:27 -0700

    not import files, but cannot merge before commiting

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import TestHome from './testHome';
+import APP from './App';
 import './index.css'
 import { Provider } from 'react-redux';
 import store from './store/store';
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+
+      <TestHome />
     </Provider>
   </React.StrictMode>,
 )
